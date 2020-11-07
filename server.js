1a0a20e58ab0c50c8f2a74d8a24fb3eb0112d4a8
Derli Machado <dam.machado1@gmail.com>
2020-11-07 07:41:26 -0300

    Starting the project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 app.get("/", (request, response) => {
-    response.send('Hello');
+    response.send('Server started ');
 })
 
 app.listen(3000);
