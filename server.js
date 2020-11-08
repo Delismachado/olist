af4eeca62c1d4a6e7b8a17e7ed5e237678319eff
Derli Machado <dam.machado1@gmail.com>
2020-11-08 19:47:45 -0300

    Fix: Filter and delete services.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -8,6 +9,7 @@ const app = express();
 
 //Envia dados em formato json
 app.use(express.json());
+app.use(cors());
 
 //Iniciar DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -6,10 +6,25 @@ const Category = mongoose.model('Category');
 module.exports = {
     //List all the products
     async index(req, res) {
-        const products = await Product.find();
+        let filters = {};
+        // converte valor para regex (pesquisa registros contendo valor)
+        // TODO: fazer busca textual https://docs.mongodb.com/manual/text-search/
+        if (!!req.query.name) {
+            filters.name = new RegExp(req.query.name);
+        }
+        if (!!req.query.description) {
+            filters.description = new RegExp(req.query.description);
+        }
+        if (!!req.query.priceFrom && !!req.query.priceTo) {
+            filters.price = { $gte: 25, $lte: 50 }
+        };
+
+        if (!!req.query.categoryId) {
+            filters.categories = req.query.categoryId;
+        }
+        const products = await Product.find(filters);
         return res.json(products);
     },
-
 
     //List one products
     async show(req, res) {
